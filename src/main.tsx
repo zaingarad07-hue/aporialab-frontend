445d09024e9439fb2d52f69868a862d177c6a7e7
zaingarad07-hue <zaingarad07@gmail.com>
2026-04-26 02:33:15 +0400

    feat: Wrap App in GoogleOAuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App.tsx';
 import { initSentry } from './sentry';
@@ -7,8 +8,12 @@ import { initSentry } from './sentry';
 // Initialize Sentry before rendering
 initSentry();
 
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 );
